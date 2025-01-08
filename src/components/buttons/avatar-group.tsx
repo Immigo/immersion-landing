841810d0e579578d1d4b7088479add59b909b0f0
ryanjeon <yejeon2020@u.northwestern.edu>
2025-01-08 14:21:00 -0800

    [Landing] Fix avatar

--- src/components/buttons/avatar-group.tsx
+++ src/components/buttons/avatar-group.tsx
@@ -7,32 +7,32 @@ export const AvatarGroup = (props : {
         <div className="isolate flex justify-center -space-x-2">
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FEofnNvqRtGeh1PG9iDSd37tjcCl1_profile_picture?alt=media&token=e8dddb0d-29e2-437a-9959-38047314007c"
             className="relative z-50 inline-block size-10 rounded-full ring-2 ring-white"
             />
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FgqDv9cdeQfYrjojxYhPhHEK5r9o2_profile_picture?alt=media&token=47bd6146-a61d-4d31-a694-5c689ba83be9"
             className="relative z-40 inline-block size-10 rounded-full ring-2 ring-white"
             />
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FtMTlVSBRYub0K6xD4MLTGyp0UOZ2_profile_picture?alt=media&token=1225fb1b-9fc7-4cce-957f-100ecc7752ee"
             className="relative z-30 inline-block size-10 rounded-full ring-2 ring-white"
             />
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FCOrTQvG3LtP2xEunVATBFpfmln22_profile_picture?alt=media&token=f372cd2f-f5f5-4ed1-9de2-7a7f9362c44f"
             className="relative z-20 inline-block size-10 rounded-full ring-2 ring-white"
             />
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FBKRy7pNX7KS9IRADaQxp3acSUU43_profile_picture?alt=media&token=501de254-21e0-4cc4-b97e-d1b98161341d"
             className="relative z-10 inline-block size-10 rounded-full ring-2 ring-white"
             />
             <img
             alt=""
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src="https://firebasestorage.googleapis.com/v0/b/immigo-application-8ba25.appspot.com/o/profile_pictures%2FO39r83Ik72MDYys6mCiv9u90hVM2_profile_picture?alt=media&token=9913a15d-b185-438b-8142-438a2cd69041"
             className="relative z-0 inline-block size-10 rounded-full ring-2 ring-white"
             />
         </div>
