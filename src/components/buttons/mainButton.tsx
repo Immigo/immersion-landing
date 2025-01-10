4ebf0e72a3ba0cd8b6cf3b0844a86d56cf60f1fc
ryanjeon <yejeon2020@u.northwestern.edu>
2025-01-10 12:44:11 -0800

    [Landing] Launched trial

--- src/components/buttons/mainButton.tsx
+++ src/components/buttons/mainButton.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
 export const MainButton = (props : {
     text : string,
     onClick : () => void
 }) => {
     const {text, onClick} = props;
     return (
-        <button onClick={() => onClick()} className="font-mono border-white border px-4 py-2 text-md text-white font-medium hover:bg-white hover:text-black">{text}</button>
+        <button onClick={() => onClick()} className="font-mono border-white border px-4 py-2 text-md text-white font-medium hover:bg-white hover:text-black flex justify-center items-center">
+            <span>{text}</span><ArrowRightIcon className="w-5 h-5 ml-4" />
+        </button>
     )
 }
